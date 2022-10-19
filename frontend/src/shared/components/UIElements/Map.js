e6e9cb2a01953c5016d774f57e59d773333b8b4f
Ganzorig2022 <ganzo.galaxy@gmail.com>
2022-10-19 16:46:36 +0800

    mongodb connected to front end

--- frontend/src/shared/components/UIElements/Map.js
+++ frontend/src/shared/components/UIElements/Map.js
@@ -2,19 +2,20 @@ import React, { useRef, useEffect } from 'react';
 
 import './Map.css';
 
-const Map = props => {
+//map javascript api key-gee public/index.html dotor bairluulsnaar ajillana.
+const Map = (props) => {
   const mapRef = useRef();
-  
+
   const { center, zoom } = props;
 
   useEffect(() => {
     const map = new window.google.maps.Map(mapRef.current, {
       center: center,
-      zoom: zoom
+      zoom: zoom,
     });
-  
+
     new window.google.maps.Marker({ position: center, map: map });
-  }, [center, zoom]);  
+  }, [center, zoom]);
 
   return (
     <div
