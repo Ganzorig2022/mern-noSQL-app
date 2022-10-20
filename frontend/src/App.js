4aaa3305904706e680630a6041610902c7fa3dff
Ganzorig2022 <ganzo.galaxy@gmail.com>
2022-10-20 16:17:17 +0800

    auto login, auto logout added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -14,19 +14,74 @@ import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
 
+let logoutTimer;
+
 const App = () => {
   const [token, setToken] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState(false);
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
+
+    //current date+ (1000*60*60) => 1 hour added
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(tokenExpirationDate);
+
+    //keep logged in when page refreshes
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        //Thu Oct 20 2022 16:33:29 GMT+0800 (Ulaanbaatar Standard Time) -->
+        // --> 2022-10-20T08:32:55.118Z by ".toISOString()" method
+        expiration: tokenExpirationDate.toISOString(),
+      })
+    );
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
+    setTokenExpirationDate(null);
     setUserId(null);
+
+    //remove token from local.
+    localStorage.removeItem('userData');
   }, []);
+
+  //1 hour later token will expire, then user will be logged out automatically.
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      //FutureDate => tokenExpirationDate.getTime() --> Thu Oct 20 2022 16:50:19 GMT+0800 (Ulaanbaatar Standard Time).getTime() -->
+      // -->1666255775674 millisecond.
+      // currentDate => new Date().getTime()
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  //keep logged in when page refreshes
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
+    }
+  }, [login]);
 
   let routes;
 
